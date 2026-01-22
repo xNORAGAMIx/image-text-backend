b78d892287ef7295718bce8cb8490f21fde208f3
Manas Das <noragami0021@gmail.com>
2026-01-22 18:32:51 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -3,6 +3,21 @@ from app.routes import upload
 
 app = FastAPI(title="Image + Text Backend")
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(upload.router)
 
 @app.get("/")
